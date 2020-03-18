ab6798516b879485c837da7796f29a159857f28d
Ioseph Kim <iosephkim@gmail.com>
2020-03-19 00:20:48 +0900

    Update 3.executeall.sh

--- 3.executeall.sh
+++ 3.executeall.sh
@@ -4,6 +4,6 @@
 for i in `docker ps -a | awk '{print $NF}' | grep postgresql- | sort -t '-' -k 2 -n`
 do
 	echo -e "\033[01;34m[$i]\033[00m"
-	docker exec -it -e PGUSER=postgres -e PGDATABASE=template1 $i "$@"
+	docker exec -e "COLUMNS=$COLUMNS" -it -e PGUSER=postgres -e PGDATABASE=template1 $i "$@"
 	echo "================================================================================"
 done
